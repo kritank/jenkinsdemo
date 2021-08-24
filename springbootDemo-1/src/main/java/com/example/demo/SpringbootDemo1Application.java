4a44a0fe9a0ba9db1f8ea6de7a67718f4f1980fb
Administrator <Administrator@host.docker.internal>
2021-08-25 01:04:11 +0530

    second commit

--- springbootDemo-1/src/main/java/com/example/demo/SpringbootDemo1Application.java
+++ springbootDemo-1/src/main/java/com/example/demo/SpringbootDemo1Application.java
@@ -8,6 +8,7 @@ public class SpringbootDemo1Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootDemo1Application.class, args);
+		System.out.println("Added syso");
 	}
 
 }
